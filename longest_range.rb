04b6e1fd6dcd5ef8eb62a5351c20bdd595ab957c
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-12 10:53:26 +0100

    changes to longest_range

--- longest_range.rb
+++ longest_range.rb
@@ -1,7 +1,10 @@
 # Longest Range
 # https://github.com/WomenWhoCodeNYC/Algorithms/blob/master/challenges/longestRange/longestRange.md
+# Write a function that accepts as input an array of integers and then returns the length
+# of the longest consecutive range of integers that appear somewhere in that array.
+# Write another function that returns the numbers in the range themselves
 
-# first define a sort method
+# sort the array
 def bubble_sort(arr)
   sorted = false
   swaps = 0
@@ -30,13 +33,15 @@ def longest_range(arr)
   bubble_sort(arr)
   new_arr = []
   i = 0
+  w = 0
+  count = 0
   until i == arr.length
     if arr[i] - 1 == arr[i-1]
-      new_arr << arr[i]
+      w += 1
     end
-    i += 1
+    
   end
-  new_arr
+  count + 1
 end
 
 arr = [16, 6, 12, 5, 4, 10, 2, 11, 13, 3, 15]
