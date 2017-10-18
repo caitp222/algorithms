f0f9a10b1ae6adb33a31fcc810f4a97cea73d470
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-18 12:22:24 +0100

    add inplace_merge two sorted arrays

---
+++ inplace_merge.rb
@@ -0,0 +1,32 @@
+# to be done in place and not using any other data structure
+
+def inplace_merge(arr1, arr2)
+  i = 0
+  w = 0
+  while i < arr1.length
+    while w < arr2.length
+      if arr1[i] > arr2[w]
+        temp = arr1[i]
+        arr1[i] = arr2[w]
+        arr2[w] = temp
+        sort_array(arr2)
+      end
+      w += 1
+    end
+    i += 1
+    w = 0
+  end
+  "x: #{arr1}, Y:#{arr2}"
+end
+
+def sort_array(arr)
+  # assuming only one element is out of position
+  i = 0
+  while i < arr.length - 1
+    if arr[i] > arr[i+1]
+      arr[i], arr[i+1] = arr[i+1], arr[i]
+    end
+    i += 1
+  end
+  arr
+end
