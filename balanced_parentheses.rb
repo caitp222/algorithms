5f47a0771fbf97aee19e717807fc2b38af975770
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-26 17:51:39 -0400

    add code for balanced_parentheses

--- balanced_parentheses.rb
+++ balanced_parentheses.rb
@@ -1,8 +1,17 @@
-def balanced_parentheses?(str, min=0, max=str.length-1)
-  if !min || !max
-    return false
-  elsif 
+def balanced_parentheses?(str)
+  arr = []
+  i = 0
+  while i < str.length
+    if str[i] == "("
+      arr << "("
+    elsif str[i] == ")" && arr.last == "("
+      arr.pop
+    else
+      return false
+    end
+    i += 1
   end
+  arr.empty?
 end
 
 
