dedf003cef7537063dbe62e423b3f3cbd9e1a421
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-11-29 22:52:44 -0500

    add test case to bubble sort ruby

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,11 +11,13 @@ def bubble_sort(arr)
       i += 1
     end
     if swaps > 0
-      sorted = false
       swaps = 0
       i = 0
     elsif swaps == 0
       sorted = true
     end
   end
+  arr
 end
+
+p bubble_sort([156,6,8,1,3,4,0,14])
