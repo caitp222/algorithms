8364e87d42abd82a22b3da2a48ab477c525eee36
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-12-15 14:30:55 -0500

    almost there

--- sudoku/solved_checker.py
+++ sudoku/solved_checker.py
@@ -1,36 +1,3 @@
-# def is_solved(st):
-#     sorted_arr = ["1","2","3","4","5","6","7","8","9"]
-#     matrix_rows = []
-#     for i in range(9):
-#         row = []
-#         for j in range(9):
-#             row.append(st[i * 9 + j])
-#         matrix_rows.append(row)
-#     matrix_columns = []
-#     for i in range(9):
-#         column = []
-#         for j in range(9):
-#             column.append(matrix_rows[j][i])
-#         matrix_columns.append(column)
-#     matrix_boxes = []
-#     for h in range(3):
-#         for i in range(3):
-#             box = []
-#             for j in range(3):
-#                 for k in range(3):
-#                     box.append(matrix_rows[3 * i + j][3 * h + k])
-#             matrix_boxes.append(box)
-#     for matrix in matrix_columns:
-#         if matrix.sort() != sorted_arr:
-#             return False
-#     for matrix in matrix_rows:
-#         if matrix.sort() != sorted_arr:
-#             return False
-#     for matrix in matrix_boxes:
-#         if matrix.sort() != sorted_arr:
-#             return False
-#     return True
-
 import copy
 
 def is_solved(arr):
--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -2,52 +2,36 @@ from pretty_board import pretty_board, pretty_matrix
 from solved_checker import is_solved
 from solution_helpers import create_matrix, create_box_possibilities, populate_possibilities, flatten_matrix
 
+def sudoku_handle(puzzle):
+    populate_possibilities(puzzle)
+    while flatten_matrix(puzzle) > 0:
+        print "sudoku handle\n"
+        populate_possibilities(puzzle)
+        pretty_matrix(puzzle)
+    return is_solved(puzzle)
+
+import copy
+
 def sudoku_solve(puzzle):
     if type(puzzle) == str:
         puzzle = create_matrix(puzzle)
-    pretty_matrix(puzzle)
-    populate_possibilities(puzzle)
-    while flatten_matrix(puzzle) > 0:
-        print "\n"
-        flatten_matrix(puzzle)
-        populate_possibilities(puzzle)
-        pretty_matrix(populate_possibilities(puzzle))
-    assigned = False
+    sudoku_handle(puzzle)
+    new_puzzle = copy.deepcopy(puzzle)
     for i in range(len(puzzle)):
         for j in range(len(puzzle[0])):
             if len(puzzle[i][j]) > 1:
-                # print puzzle[i][j]
                 possibles = puzzle[i][j]
-                assigned = True
-                break
-        if assigned:
-            break
-    puzzle[i][j] = [possibles[1]]
-    pretty_matrix(puzzle)
-    while flatten_matrix(puzzle) > 0:
-        print "\n"
-        flatten_matrix(puzzle)
-        populate_possibilities(puzzle)
-        pretty_matrix(populate_possibilities(puzzle))
-    assigned = False
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[0])):
-            if len(puzzle[i][j]) > 1:
-                # print puzzle[i][j]
-                possibles = puzzle[i][j]
-                assigned = True
-                break
-        if assigned:
-            break
-    puzzle[i][j] = [possibles[0]]
-    pretty_matrix(puzzle)
-    while flatten_matrix(puzzle) > 0:
-        print "\n"
-        flatten_matrix(puzzle)
-        populate_possibilities(puzzle)
-        pretty_matrix(populate_possibilities(puzzle))
-    print is_solved(puzzle)
-
+                print possibles
+                for possiblity in possibles:
+                    puzzle[i][j] = possiblity
+                    if sudoku_handle(puzzle) == True:
+                        return True
+                    else:
+                        print "blah"
+                        puzzle = copy.deepcopy(new_puzzle)
+                        print puzzle[i][j]
+                        pretty_matrix(puzzle)
+    return False
 
 
 puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
@@ -56,5 +40,5 @@ puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5
 # puzzle = "-8--2-----4-5--32--2-3-9-466---9---4---64-5-1134-5-7--36---4--24-723-6-----7--45-"
 # puzzle = "6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--"
 # pretty_matrix(sudoku_solve(puzzle))
-sudoku_solve(puzzle)
+print sudoku_solve(puzzle)
 # print sudoku_solve(puzzle)
