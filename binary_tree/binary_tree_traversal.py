7fc2ebd66ff79599fca3aacdf215409f6a39f830
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-12-19 12:24:02 -0500

    add code for recursive inorder traversal

--- binary_tree/binary_tree_traversal.py
+++ binary_tree/binary_tree_traversal.py
@@ -1,7 +1,7 @@
 # http://interactivepython.org/runestone/static/pythonds/Trees/NodesandReferences.html
 # http://www.techiedelight.com/inorder-tree-traversal-iterative-recursive/
 
-class BinaryTree:
+class BinaryTreeNode:
   def __init__(self, data):
     self.data = data
     self.left = None
@@ -9,25 +9,47 @@ class BinaryTree:
 
   def insert_left(self, data):
     if self.left == None:
-      self.left = BinaryTree(data)
+      self.left = BinaryTreeNode(data)
     else:
       self.left.insert_left(data)
 
-  def insert_right(self,data):
+  def insert_right(self, data):
     if self.right == None:
-      self.right = BinaryTree(data)
+      self.right = BinaryTreeNode(data)
     else:
       self.right.insert_right(data)
 
-  def preorder_traversal(self):
+  def inorder_traversal_iterative(self):
+      pass
+
+  def inorder_traversal_recursive(self):
+      if self == None:
+          return self
+      if self.left != None:
+          self.left.inorder_traversal_recursive()
+      print self.data
+      if self.right != None:
+          self.right.inorder_traversal_recursive()
+
+  def preorder_traversal_iterative(self):
+      pass
+
+  def preorder_traversal_recursive(self):
+      print self.data
       if self.left:
           self.left.preorder_traversal()
       if self.right:
           self.right.preorder_traversal()
 
+  def postorder_traversal_iterative(self):
+      pass
+
+  def postorder_traversal_recursive(self):
+      pass
+
 
 # build tree
-tree = BinaryTree(1)
+tree = BinaryTreeNode(1)
 tree.insert_left(2)
 tree.insert_left(4)
 tree.insert_right(3)
@@ -35,4 +57,5 @@ tree.insert_right(6)
 tree.right.insert_left(5)
 tree.right.insert_left(7)
 tree.right.left.insert_right(8)
-tree.preorder_traversal()
+tree.inorder_traversal_recursive()
+# print(tree.inorder_traversal_recursive())
