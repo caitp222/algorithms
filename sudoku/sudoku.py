3512800bf4da5b09e3dcdae7dc44e26b197d25e2
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-12-24 15:35:19 +0000

    adjust code to solve 7th puzzle

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -8,9 +8,11 @@ def sudoku_solve(puzzle):
     if type(puzzle) == str:
         puzzle = create_matrix(puzzle)
     populate_possibilities(puzzle)
+    # pretty_matrix(puzzle)
     while flatten_matrix(puzzle) > 0:
         populate_possibilities(puzzle)
     if is_solved(puzzle):
+        print "solved!!"
         pretty_matrix(puzzle)
         return True
     new_puzzle = copy.deepcopy(puzzle)
@@ -20,9 +22,19 @@ def sudoku_solve(puzzle):
                 possibles = new_puzzle[i][j]
                 for possibility in possibles:
                     new_puzzle[i][j] = possibility
+                    # print "blooooooop"
+                    # pretty_matrix(new_puzzle)
                     if sudoku_solve(new_puzzle) == True:
                         return True
+                    else:
+                        continue
+            elif len(new_puzzle[i][j]) < 1:
+                new_puzzle = copy.deepcopy(puzzle)
+                continue
     return False
 
 puzzle = "-3-5--8-45-42---1---8--9---79-8-61-3-----54---5------78-----7-2---7-46--61-3--5--"
+print sudoku_solve(puzzle)
+
+puzzle = "-96-4---11---6---45-481-39---795--43-3--8----4-5-23-18-1-63--59-59-7-83---359---7"
 print sudoku_solve(puzzle)
